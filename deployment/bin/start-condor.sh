ea93c8574df90395b483f503fc244c90097d2fd4
bio-boris <bio-boris@users.noreply.github.com>
2019-03-07 13:37:43 -0600

    Update start-condor.sh

--- deployment/bin/start-condor.sh
+++ deployment/bin/start-condor.sh
@@ -16,4 +16,11 @@ if [ "$SET_NOBODY_USER_UID" ] ; then
     usermod -u "$SET_NOBODY_USER_UID" nobody -o
 fi
 
+if [ "$condor_submit_workdir" ] ; then
+    mkdir -p $ condor_submit_workdir
+else
+    mkdir -p /mnt/awe/condor/condor_job_execute
+fi
+
+
 exec /usr/sbin/condor_master -f -t 2>&1 
