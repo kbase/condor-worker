ff0691aab7f6527c821f1d9834af3d4ffe15463b
Boris <bio-boris@github.com>
2023-02-07 10:24:38 -0600

    Add more configs

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM htcondor/execute:9.12.0-el7
 ENV container docker
-COPY pre-exec.sh /root/config/pre-exec.sh
+
 # See https://www-auth.cs.wisc.edu/lists/htcondor-users/2014-August/msg00044.shtml
+COPY pre-exec.sh /root/config/pre-exec.sh
 COPY kbase_worker.conf /etc/condor/condor_config.local
 
 # Get commonly used utilities
--- pre-exec.sh
+++ pre-exec.sh
@@ -2,7 +2,6 @@
 
 # See documentation at https://github.com/htcondor/htcondor/tree/main/build/docker/services#providing-additional-configuration
 
-
 if [ "$CONDOR_JWT_TOKEN" ] ; then
     echo "$CONDOR_JWT_TOKEN" > /etc/condor/tokens.d/JWT
     chmod 600 /etc/condor/tokens.d/JWT
@@ -10,5 +9,27 @@ fi
 
 if [ "$COLLECTOR_HOST" ] ; then
     echo "COLLECTOR_HOST = $COLLECTOR_HOST" >> /etc/condor/condor_config.local
-    /update-config
 fi
+
+if [ "$CLIENTGROUP" ] ; then
+    echo "CLIENTGROUP = \"$CLIENTGROUP\"" >> /etc/condor/condor_config.local
+fi
+
+# To keep docker partition from filling up
+if [ "$DOCKER_SYSTEM_PRUNE" ] ; then
+    docker system prune -a -f
+fi
+
+
+#TODO Make nobody user able to run jobs
+
+
+# Ensure condor user can write to logs, since these are mounted onto host
+chown condor $(condor_config_val log) $(condor_config_val lock) $(condor_config_val run)
+
+
+
+
+
+
+/update-config
