bcc13a6d15eac8365abbc3c42f709a451eacda92
Boris <bio-boris@github.com>
2023-02-15 16:26:19 -0600

    Permissions

--- pre-exec.sh
+++ pre-exec.sh
@@ -2,6 +2,7 @@
 
 # See documentation at https://github.com/htcondor/htcondor/tree/main/build/docker/services#providing-additional-configuration
 
+######################## Required Values BEGIN ########################################
 if [ "$CONDOR_JWT_TOKEN" ] ; then
     echo "$CONDOR_JWT_TOKEN" > /etc/condor/tokens.d/JWT
     chmod 600 /etc/condor/tokens.d/JWT
@@ -10,6 +11,13 @@ fi
 if [ "$COLLECTOR_HOST" ] ; then
     echo "COLLECTOR_HOST = $COLLECTOR_HOST" >> /etc/condor/condor_config.local
 fi
+
+# Change execute directory for multiple works on the same host, instead of the default /cdr/ dir
+if [ "$EXECUTE" ] ; then
+  echo "EXECUTE = $EXECUTE >> /etc/condor/condor_config.local"
+
+fi
+######################## Required Values END ##########################################
 
 #Note the clientgroup will require quotation marks in the env variable
 if [ "$CLIENTGROUP" ] ; then
@@ -22,25 +30,17 @@ if [ "$DOCKER_SYSTEM_PRUNE" ] ; then
 fi
 
 
+
+
+
 #TODO Make nobody user able to run jobs
 ####################### HOST PATHS ############################################
 DIRS_TO_CREATE=$(condor_config_val DIRS_TO_CREATE)
 mkdir -p $DIRS_TO_CREATE
-chmod 770 $DIRS_TO_CREATE && chown root:condor $DIRS_TO_CREATE
+chmod 01777 $DIRS_TO_CREATE && chown root:condor $DIRS_TO_CREATE
+
 # /condor/execute root-squashed or not condor-owned: requiring world-writability
 chmod 01777 $(condor_config_val EXECUTE)
-chmod 01777 /var/run/docker.sock
-/update-config
+chmod 01777 $(condor_config_val DOCKER_SOCKET)
 ####################### HOST PATHS ############################################
-
-
-
-# Ensure condor user can write to logs, since these are mounted onto host
-chown condor $(condor_config_val log) $(condor_config_val lock) $(condor_config_val run)
-
-
-
-
-
-
 /update-config
